d36d04ceccb5065acba11ded86901f13cd46bc66
Claire Sial and Margie Nevarez <claire@>
2017-01-31 13:12:42 -0800

    add detail/design to bill and legislator results

--- src/app/bill-results/bill-results.component.ts
+++ src/app/bill-results/bill-results.component.ts
@@ -3,12 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bill-results',
   templateUrl: './bill-results.component.html',
-  styleUrls: ['./bill-results.component.css']
+  styleUrls: ['./bill-results.component.scss'],
 })
 export class BillResultsComponent implements OnInit {
 
   constructor() { }
-
+  
   ngOnInit() {
   }
 
--- src/app/bill-search-topic/bill-search-topic.component.ts
+++ src/app/bill-search-topic/bill-search-topic.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bill-search-topic',
   templateUrl: './bill-search-topic.component.html',
-  styleUrls: ['./bill-search-topic.component.css']
+  styleUrls: ['./bill-search-topic.component.scss']
 })
 export class BillSearchTopicComponent implements OnInit {
 
@@ -13,6 +13,6 @@ export class BillSearchTopicComponent implements OnInit {
   }
 
   searchBillCommittee(selectedCommittee) {
-    
+
   }
 }
