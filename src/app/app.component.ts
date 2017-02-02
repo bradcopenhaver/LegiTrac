7ddd0ec454046eff6a3460c6a70d0fb01150a833
Kenneth Black <kenneth.black@live.com>
2017-02-02 10:51:59 -0800

    add icon click, deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  constructor(private router: Router) {}
+
+
+  goHome() {
+    this.router.navigate([''])
+  }
 }
